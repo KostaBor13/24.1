37fcc81b144e4af1b66988a335c90680c360eb1e
Kostas <zaikin399@mail.ru>
2024-07-06 16:16:04 +0300

    выполнение дз 24.2

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from materials.models import Course, Lesson
 
@@ -59,28 +60,33 @@ class Payment(models.Model):
         verbose_name='пользователь',
         help_text='выберите пользователя',
         **NULLABLE
+
     )
     date_of_payment = models.DateTimeField(
         auto_now_add=True,
         verbose_name='дата оплаты',
         **NULLABLE
+
     )
     paid_course = models.ForeignKey(
         Course,
         on_delete=models.CASCADE,
         verbose_name='оплаченный курс',
-        **NULLABLE
+        **NULLABLE,
+        related_name='paid_course_payments'
     )
     paid_lesson = models.ForeignKey(
         Lesson,
         on_delete=models.CASCADE,
         verbose_name='оплаченный урок',
-        **NULLABLE
+        **NULLABLE,
+        related_name='paid_lesson_payments'
     )
     payment_amount = models.PositiveIntegerField(
         verbose_name='сумма оплаты',
         help_text='введите сумму оплаты',
         **NULLABLE
+
     )
     payment_method = models.CharField(
         max_length=20,
@@ -88,6 +94,20 @@ class Payment(models.Model):
         verbose_name='способ оплаты',
         help_text='выберите способ оплаты'
     )
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True, related_name='course_payments')
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True, blank=True, related_name='lesson_payments')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        course = cleaned_data.get('course')
+        lesson = cleaned_data.get('lesson')
+
+        if course and lesson:
+            raise ValidationError('Укажите только одно поле: либо курс, либо урок.')
+        elif not course and not lesson:
+            raise ValidationError('Одно из полей должно быть заполнено.')
+
+        return cleaned_data
 
     def __str__(self):
         return f'''{self.user}: {self.date_of_payment}, {self.payment_amount}, {self.payment_method}
